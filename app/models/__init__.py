4604e84384ba7b1a7965c466012fd5613319674d
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-08-12 12:41:07 +0500

    add attendance class to import in models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,4 +3,4 @@ from .employee_image import EmployeeImage
 from .filial import Filial
 from .position import Position
 from .working_graphic import WorkingGraphic
-from attendance import Attendance
+from .attendance import Attendance
