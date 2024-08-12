20118f96c8b58b4ab819bc2a55048a71841d933e
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-08-12 17:08:22 +0500

    add user to import in api

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -5,6 +5,7 @@ from .filial import router as filial_router
 from .employee_image import router as employee_image_router
 from .position import router as position_router
 from .working_graphic import router as working_graphic_router
+from .user import router as user_router
 
 router = APIRouter()
 router.include_router(employees_router, prefix="/employees", tags=["Employees"])
@@ -13,3 +14,4 @@ router.include_router(filial_router, prefix="/filials", tags=["Filials"])
 router.include_router(position_router, prefix="/positions", tags=["Positions"])
 router.include_router(working_graphic_router, prefix="/working_graphics", tags=["Working Graphics"])
 router.include_router(attendance_router, prefix="/attendance", tags=["Attendance"])
+router.include_router(user_router, prefix="/users", tags=["Users"])
