fde72b256f2455c5d4c3deacd12a0aee2155f99a
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-08-12 17:08:31 +0500

    add user to import in crud

--- app/crud/__init__.py
+++ app/crud/__init__.py
@@ -4,3 +4,4 @@ from .position import create_position, get_positions, get_position, update_posit
 from .employee_image import create_employee_image, get_employees_images, get_employee_images, update_employee_image, delete_employee_image, get_employee_image
 from .working_graphic import create_working_graphic, get_working_graphics, get_working_graphic, update_working_graphic, delete_working_graphic
 from .attendance import create_attendance, get_attendances
+from .user import create_user, login_user, get_users, get_user, update_user, delete_user
