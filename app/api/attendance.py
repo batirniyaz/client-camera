22783a212bbbabb2cdeb20c9c4c3ea21b0a3929c
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-08-12 11:44:30 +0500

    create first version of create_attendance_endpoint func in api.attendance

--- app/api/attendance.py
+++ app/api/attendance.py
@@ -1,4 +1,20 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from ..crud.attendance import create_attendance
+from ..database import get_db
+from ..schemas import AttendanceCreate
 
 router = APIRouter()
+
+
+@router.post("/")
+async def create_attendance_endpoint(attendance: AttendanceCreate, file: UploadFile, db: AsyncSession = Depends(get_db)):
+    """
+    Create a new attendance with the given details.
+    :param attendance:
+    :param file:
+    :param db:
+    :return:
+    """
+    return await create_attendance(db, attendance, file)
