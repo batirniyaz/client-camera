92a03f58e26cc88e33cb2eaca6d486906b4587a5
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-08-12 11:19:02 +0500

    change attendance.py model for correct version

--- app/models/attendance.py
+++ app/models/attendance.py
@@ -1,12 +1,20 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+import datetime
+
+from sqlalchemy import ForeignKey, text
+from sqlalchemy.orm import Mapped, mapped_column
 from ..database import Base
+
 
 class Attendance(Base):
     __tablename__ = 'attendances'
 
-    id = Column(Integer, primary_key=True, index=True)
-    employee_id = Column(Integer, ForeignKey("employees.id"))
-    score = Column(Integer)
-    datetime = Column(String)
-    created_at = Column(String)
-    updated_at = Column(String)
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    person_id: Mapped[int] = mapped_column(ForeignKey('employees.id'))
+    file_path: Mapped[str] = mapped_column()
+    camera_id: Mapped[int] = mapped_column()
+    time: Mapped[str] = mapped_column()
+    score: Mapped[str] = mapped_column()
+
+    created_at: Mapped[datetime.datetime] = mapped_column(server_default=text("TIMEZONE('utc', now())"))
+    updated_at: Mapped[datetime.datetime] = mapped_column(server_default=text("TIMEZONE('utc', now())"), onupdate=text("TIMEZONE('utc', now())"))
+
