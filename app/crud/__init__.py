887d43b19c244e94cc157739fd84be024b6cca40
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-08-12 14:35:25 +0500

    add get_attendances to import in crud

--- app/crud/__init__.py
+++ app/crud/__init__.py
@@ -3,4 +3,4 @@ from .filial import create_filial, get_filials, get_filial, update_filial, delet
 from .position import create_position, get_positions, get_position, update_position, delete_position
 from .employee_image import create_employee_image, get_employees_images, get_employee_images, update_employee_image, delete_employee_image, get_employee_image
 from .working_graphic import create_working_graphic, get_working_graphics, get_working_graphic, update_working_graphic, delete_working_graphic
-from .attendance import create_attendance
+from .attendance import create_attendance, get_attendances
