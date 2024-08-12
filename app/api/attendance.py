945f5863405f2bd337a62e37d390a3ffc3d067f5
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-08-12 14:06:04 +0500

    change for query request from json

--- app/api/attendance.py
+++ app/api/attendance.py
@@ -1,20 +1,32 @@
-from fastapi import APIRouter, UploadFile, Depends
+from fastapi import APIRouter, UploadFile, Depends, File
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from ..crud.attendance import create_attendance
 from ..database import get_db
-from ..schemas import AttendanceCreate
+from ..schemas import AttendanceCreate, AttendanceResponse
 
 router = APIRouter()
 
 
-@router.post("/")
-async def create_attendance_endpoint(attendance: AttendanceCreate, file: UploadFile, db: AsyncSession = Depends(get_db)):
+@router.post("/", response_model=AttendanceResponse)
+async def create_attendance_endpoint(
+        person_id: int,
+        camera_id: int,
+        time: str,
+        score: str,
+
+        file: UploadFile = File(...),
+        db: AsyncSession = Depends(get_db),
+
+):
     """
     Create a new attendance with the given details.
-    :param attendance:
+    :param score:
+    :param time:
+    :param person_id:
+    :param camera_id:
     :param file:
     :param db:
     :return:
     """
-    return await create_attendance(db, attendance, file)
+    return await create_attendance(db, file, person_id, camera_id, time, score)
