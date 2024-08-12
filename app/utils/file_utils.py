2ec278908975bee9ae1d8f72cb42e508554048ff
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-08-12 12:41:55 +0500

    configure file_utils.py to reach requirements

--- app/utils/file_utils.py
+++ app/utils/file_utils.py
@@ -6,9 +6,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 IMAGE_DIR = BASE_DIR / "storage" / "users"
 
 
-def save_upload_file(upload_file: UploadFile) -> str:
-    IMAGE_DIR.mkdir(parents=True, exist_ok=True)
-    file_location = IMAGE_DIR / upload_file.filename
+def save_upload_file(upload_file: UploadFile, employee_id: int) -> str:
+    employee_dir = IMAGE_DIR / str(employee_id) / "images"
+    employee_dir.mkdir(parents=True, exist_ok=True)
+    file_location = employee_dir / upload_file.filename
     with open(file_location, "wb") as buffer:
         buffer.write(upload_file.file.read())
     return upload_file.filename
