501d6b920aa31dd279ae21b2ee5c851226e6d051
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-08-12 17:08:50 +0500

    add user to import in schemas

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -4,3 +4,4 @@ from .filial import FilialBase, FilialCreate, FilialUpdate, FilialResponse
 from .position import PositionBase, PositionCreate, PositionUpdate, PositionResponse
 from .working_graphic import WorkingGraphicBase, WorkingGraphicCreate, WorkingGraphicUpdate, WorkingGraphicResponse
 from .attendance import AttendanceBase, AttendanceCreate, AttendanceUpdate, AttendanceResponse, AttendanceDataResponse
+from .user import UserBase, UserCreate, UserUpdate, UserResponse
