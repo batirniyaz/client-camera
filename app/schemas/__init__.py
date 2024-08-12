bfa59c621a023b2f9cf581899970d6ff2b4cddec
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-08-12 11:32:34 +0500

    add AttendanceBase, AttendanceCreate, AttendanceUpdate, AttendanceResponse classes to import

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -3,3 +3,4 @@ from .employee_image import EmployeeImageBase, EmployeeImageCreate, EmployeeImag
 from .filial import FilialBase, FilialCreate, FilialUpdate, FilialResponse
 from .position import PositionBase, PositionCreate, PositionUpdate, PositionResponse
 from .working_graphic import WorkingGraphicBase, WorkingGraphicCreate, WorkingGraphicUpdate, WorkingGraphicResponse
+from .attendance import AttendanceBase, AttendanceCreate, AttendanceUpdate, AttendanceResponse
