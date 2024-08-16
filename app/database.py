2dd88f31935cc2fcb4032905bfed1a87f3d238b3
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-08-16 15:51:10 +0500

    add BASE_URL to database.py

--- app/database.py
+++ app/database.py
@@ -5,6 +5,8 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "postgresql+asyncpg://postgres:happynation@localhost:5432/db_employee"
 
+BASE_URL = "http://37.140.216.89:8000"
+
 engine = create_async_engine(DATABASE_URL, echo=False)
 SessionLocal = async_sessionmaker(bind=engine, class_=AsyncSession, autocommit=False, autoflush=False)
 Base: DeclarativeMeta = declarative_base()
