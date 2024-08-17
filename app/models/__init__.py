fe3f078a05541ed2a913d78d9dbc394e6b46c4c1
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-08-17 13:13:15 +0500

    add imports to __init__.py file of models and schemas

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,6 +2,6 @@ from .employee import Employee
 from .employee_image import EmployeeImage
 from .filial import Filial
 from .position import Position
-from .working_graphic import WorkingGraphic
+from .working_graphic import WorkingGraphic, Day
 from .attendance import Attendance
 from .user import User
--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -2,6 +2,6 @@ from .employee import EmployeeBase, EmployeeCreate, EmployeeUpdate, EmployeeResp
 from .employee_image import EmployeeImageBase, EmployeeImageCreate, EmployeeImageUpdate, EmployeeImageResponse
 from .filial import FilialBase, FilialCreate, FilialUpdate, FilialResponse
 from .position import PositionBase, PositionCreate, PositionUpdate, PositionResponse
-from .working_graphic import WorkingGraphicBase, WorkingGraphicCreate, WorkingGraphicUpdate, WorkingGraphicResponse
+from .working_graphic import WorkingGraphicBase, WorkingGraphicCreate, WorkingGraphicUpdate, WorkingGraphicResponse, DayBase, DayCreate, DayUpdate, DayResponse
 from .attendance import AttendanceBase, AttendanceCreate, AttendanceUpdate, AttendanceResponse, AttendanceDataResponse
 from .user import UserBase, UserCreate, UserUpdate, UserResponse
