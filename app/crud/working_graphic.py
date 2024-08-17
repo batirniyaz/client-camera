004d71b2ed9dd183bcb8f42763dd60487137aa9b
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-08-17 13:15:18 +0500

    change logic of working_graphic func and add functions to day schema

--- app/crud/working_graphic.py
+++ app/crud/working_graphic.py
@@ -1,8 +1,8 @@
 from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
-from ..models.working_graphic import WorkingGraphic
-from ..schemas.working_graphic import WorkingGraphicCreate, WorkingGraphicUpdate
+from ..models.working_graphic import WorkingGraphic, Day
+from ..schemas.working_graphic import WorkingGraphicCreate, WorkingGraphicUpdate, DayCreate, DayUpdate
 
 
 async def create_working_graphic(db: AsyncSession, working_graphic: WorkingGraphicCreate):
@@ -11,6 +11,12 @@ async def create_working_graphic(db: AsyncSession, working_graphic: WorkingGraph
         db.add(db_working_graphic)
         await db.commit()
         await db.refresh(db_working_graphic)
+
+        for day in working_graphic.days:
+            db_day = Day(**day.model_dump(), working_graphic_id=db_working_graphic.id)
+            db.add(db_day)
+
+        await db.commit()
 
         return db_working_graphic
     except Exception as e:
