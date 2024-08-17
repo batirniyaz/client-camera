5d0d554a76a45f06dc82647994cdb6f24457089f
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-08-17 13:17:27 +0500

    first version of model for clients

--- app/models/client.py
+++ app/models/client.py
@@ -1,1 +1,21 @@
 
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import text
+import datetime
+
+from ..database import Base
+
+
+class Client(Base):
+    __tablename__ = 'clients'
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    gender: Mapped[str] = mapped_column()
+    score: Mapped[str] = mapped_column()
+    age: Mapped[int] = mapped_column()
+    client_status: Mapped[str] = mapped_column()
+    camera_id: Mapped[int] = mapped_column()
+
+    created_at: Mapped[datetime.datetime] = mapped_column(server_default=text("TIMEZONE('utc', now())"))
+    updated_at: Mapped[datetime.datetime] = mapped_column(server_default=text("TIMEZONE('utc', now())"),
+                                                          onupdate=text("TIMEZONE('utc', now())"))
