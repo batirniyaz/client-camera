729922e7d7a5d8c464614960b0c751b4b6c3a4a1
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-08-19 16:19:23 +0500

    create model for Day and Working graphic from zero

--- app/models/working_graphic.py
+++ app/models/working_graphic.py
@@ -1,22 +1,19 @@
 import datetime
 
-from sqlalchemy import text, ForeignKey
+from sqlalchemy import ForeignKey, text
 from sqlalchemy.orm import relationship, Mapped, mapped_column
 
-from typing import TYPE_CHECKING
-from ..database import Base
-
-if TYPE_CHECKING:
-    from .employee import Employee
+from app.database import Base
+from app.models import Employee
 
 
 class Day(Base):
-    __tablename__ = 'days'
+    __tablename__ = "days"
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     day: Mapped[str] = mapped_column()
-    time_in: Mapped[datetime.time] = mapped_column()
-    time_out: Mapped[datetime.time] = mapped_column()
+    time_in: Mapped[str] = mapped_column()
+    time_out: Mapped[str] = mapped_column()
     is_work_day: Mapped[bool] = mapped_column()
 
     working_graphic_id: Mapped[int] = mapped_column(ForeignKey("working_graphics.id"))
@@ -28,12 +25,11 @@ class Day(Base):
 
 
 class WorkingGraphic(Base):
-    __tablename__ = 'working_graphics'
+    __tablename__ = "working_graphics"
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     name: Mapped[str] = mapped_column()
-
-    days: Mapped[list[Day]] = relationship(back_populates="working_graphic", lazy="selectin")
+    days: Mapped[list["Day"]] = relationship( back_populates="working_graphic", lazy="selectin")
     employees: Mapped[list["Employee"]] = relationship(back_populates="working_graphic", lazy="selectin")
 
     created_at: Mapped[datetime.datetime] = mapped_column(server_default=text("TIMEZONE('utc', now())"))
