f542a4521eb8491885e780a11cc190dfad018a98
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-08-19 13:11:14 +0500

    update database.py: create and configure .env file

--- app/database.py
+++ app/database.py
@@ -1,11 +1,21 @@
-from sqlalchemy import create_engine
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy.ext.declarative import declarative_base, DeclarativeMeta
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
-from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+asyncpg://postgres:happynation@localhost:5432/db_employee"
+load_dotenv()
 
-BASE_URL = "http://37.140.216.89:8000"
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_NAME = os.getenv("DB_NAME")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+
+
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+BASE_URL = os.getenv("BASE_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=False)
 SessionLocal = async_sessionmaker(bind=engine, class_=AsyncSession, autocommit=False, autoflush=False)
