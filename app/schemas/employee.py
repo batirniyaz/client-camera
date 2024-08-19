5e4e5f93e8a57f6dbe2ec214d27aa70816a34cf1
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-08-19 18:31:21 +0500

    add type_checking and change response model

--- app/schemas/employee.py
+++ app/schemas/employee.py
@@ -1,8 +1,13 @@
 import datetime
 
-from pydantic import BaseModel, Field, model_validator
-from typing import List, Optional
-from .employee_image import EmployeeImageResponse
+from pydantic import BaseModel, Field
+from typing import Optional, TYPE_CHECKING
+
+# if TYPE_CHECKING:
+from app.schemas.employee_image import EmployeeImageResponse
+from app.schemas.position import PositionResponse
+from app.schemas.working_graphic import WorkingGraphicResponse
+from app.schemas.filial import FilialResponse
 
 
 class EmployeeBase(BaseModel):
@@ -30,7 +35,12 @@ class EmployeeUpdate(EmployeeBase):
 
 class EmployeeResponse(EmployeeBase):
     id: int = Field(..., description="The ID of the employee")
-    images: list[EmployeeImageResponse] = Field([], description="A list of images associated with the employee")
+    name: str = Field(..., description="The name of the employee")
+    position: 'PositionResponse' = Field(..., description="The position of the employee")
+    filial: 'FilialResponse' = Field(..., description="The filial of the employee")
+    phone_number: str = Field(..., description="The phone number of the employee")
+    working_graphic: 'WorkingGraphicResponse' = Field(None, description="The working graphic of the employee")
+    images: list['EmployeeImageResponse'] = Field([], description="A list of images associated with the employee")
 
     created_at: datetime.datetime = Field(..., description="The time the employee was created")
     updated_at: datetime.datetime = Field(..., description="The time the employee was updated")
@@ -41,25 +51,43 @@ class EmployeeResponse(EmployeeBase):
         json_schema_extra = {
             "example": {
                 "id": 1,
-                "name": "Batirniyaz",
-                "phone_number": "998905913873",
-                "position_id": 1,
-                "filial_id": 1,
+                "name": "Employee 1",
+                "position": {
+                    "id": 1,
+                    "name": "Position 1",
+                    "employees": 10,
+                    "created_at": "2024-07-25 12:00:00"
+                },
+                "filial": {
+                    "id": 1,
+                    "name": "Filial 1",
+                    "created_at": "2024-07-25 12:00:00"
+                },
+                "phone_number": "1234567890",
+                "working_graphic": {
+                    "id": 1,
+                    "name": "Working Graphic 1",
+                    "days": [
+                        {
+                            "id": 1,
+                            "day": "Monday",
+                            "time_in": "08:00:00",
+                            "time_out": "17:00:00",
+                            "is_work_day": True,
+                            "created_at": "2024-07-25 12:00:00"
+                        }
+                    ],
+                    "created_at": "2024-07-25 12:00:00"
+                },
                 "images": [
                     {
-                        "employee_id": 1,
-                        "image_url": "http://example.com/image.jpg"
+                        "id": 1,
+                        "url": "https://example.com/image.jpg",
+                        "created_at": "2024-07-25 12:00:00"
                     }
-                ]
+                ],
+                "created_at": "2024-07-25 12:00:00"
             }
         }
         arbitrary_types_allowed = True
         validate_assignment = True
-
-        # @model_validator
-        # def number_validator(cls, values):
-        #     dt = datetime.datetime.now()
-        #     if values["created_at"] is None:
-        #         values["created_at"] = dt
-        #     values["updated_at"] = dt
-        #     return values
