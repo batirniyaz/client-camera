2606c8caa50f4792a5f4323099d149f70b2c21ec
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-08-19 18:31:31 +0500

    add type_checking and change response model

--- app/crud/employee.py
+++ app/crud/employee.py
@@ -1,11 +1,14 @@
-
 from fastapi import HTTPException
 from sqlalchemy.future import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from ..models import Position, WorkingGraphic, Filial
 from ..models.employee import Employee
-from ..schemas.employee import EmployeeCreate, EmployeeUpdate
+from ..schemas.employee import EmployeeCreate, EmployeeUpdate, EmployeeResponse
+from ..schemas.position import PositionResponse
+from ..schemas.working_graphic import WorkingGraphicResponse, DayResponse
+from ..schemas.filial import FilialResponse
+from ..schemas.employee_image import EmployeeImageResponse
 
 from app.database import BASE_URL
 
@@ -27,9 +30,43 @@ async def get_employees(db: AsyncSession, skip: int = 0, limit: int = 10):
     result = await db.execute(select(Employee).offset(skip).limit(limit))
     employees = result.scalars().all()
 
+    formatted_employees = []
     for employee in employees:
-        for image in employee.images:
-            image.image_url = f"{BASE_URL}{image.image_url}"
+        formatted_employee = EmployeeResponse(
+            id=employee.id,
+            name=employee.name,
+            position=PositionResponse(
+                id=employee.position.id,
+                name=employee.position.name
+            ),
+            filial=FilialResponse(
+                id=employee.filial.id,
+                name=employee.filial.name
+            ),
+            phone_number=employee.phone_number,
+            working_graphic=WorkingGraphicResponse(
+                id=employee.working_graphic.id,
+                name=employee.working_graphic.name,
+                days=[
+                    DayResponse(
+                        id=day.id,
+                        day=day.day,
+                        time_in=day.time_in,
+                        time_out=day.time_out,
+                        is_working=day.is_work_day
+                    ) for day in employee.working_graphic.days
+                ]
+            ) if employee.working_graphic else None,
+            images=[
+                EmployeeImageResponse(
+                    id=image.image_id,
+                    url=f"{BASE_URL}{image.image_url}"
+                ) for image in employee.images
+            ],
+            created_at=employee.created_at,
+            updated_at=employee.updated_at
+        )
+        formatted_employees.append(formatted_employee)
 
     return employees
 
@@ -59,7 +96,8 @@ async def update_employee(db: AsyncSession, employee_id: int, employee: Employee
         result = await db.execute(select(WorkingGraphic).filter_by(id=employee.working_graphic_id))
         working_graphic = result.scalar_one_or_none()
         if not working_graphic:
-            raise HTTPException(status_code=400, detail=f"Working graphic with ID {employee.working_graphic_id} not found")
+            raise HTTPException(status_code=400,
+                                detail=f"Working graphic with ID {employee.working_graphic_id} not found")
 
     if employee.filial_id is not None:
         result = await db.execute(select(Filial).filter_by(id=employee.filial_id))
