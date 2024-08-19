05eb509b33fd27242c455df8d2f300fd982de9db
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-08-19 13:14:25 +0500

    change and update create_client_endpoint to handle errors

--- app/api/client.py
+++ app/api/client.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, Depends
+from datetime import datetime
+
+from fastapi import APIRouter, Depends, BackgroundTasks
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from ..crud.client import create_client, store_daily_report
+from ..crud.client import create_client, get_daily_report, make_naive, store_daily_report
 from ..schemas import ClientCreate, ClientResponse
 from ..database import get_db
 
@@ -9,7 +11,7 @@ router = APIRouter()
 
 
 @router.post("/", response_model=ClientResponse)
-async def create_client_endpoint(client: ClientCreate, db: AsyncSession = Depends(get_db)):
+async def create_client_endpoint(background_tasks: BackgroundTasks, client: ClientCreate, db: AsyncSession = Depends(get_db)):
     """
         Create a new client with the given details.
 
@@ -18,8 +20,9 @@ async def create_client_endpoint(client: ClientCreate, db: AsyncSession = Depend
         - **age**: The age of the client
         - **score**: The score of the client
         """
-    return await create_client(db, client)
 
+    created_client = await create_client(db, client, background_tasks)
+    return created_client
 
 # @router.get("{date}")
 # async def get_daily_report(date: str, db: AsyncSession = Depends(get_db)):
