3b736a38a655c4ba844c9f7b7c9e8226c8394f1c
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-08-20 16:54:07 +0500

    remove FilialResponse and PositionResponse imports to avoid error

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,7 +1,7 @@
 from .employee import EmployeeBase, EmployeeCreate, EmployeeUpdate, EmployeeResponse
 from .employee_image import EmployeeImageBase, EmployeeImageCreate, EmployeeImageUpdate, EmployeeImageResponse
-from .filial import FilialBase, FilialCreate, FilialUpdate, FilialResponse
-from .position import PositionBase, PositionCreate, PositionUpdate, PositionResponse
+from .filial import FilialBase, FilialCreate, FilialUpdate
+from .position import PositionBase, PositionCreate, PositionUpdate
 from .working_graphic import WorkingGraphicBase, WorkingGraphicCreate, WorkingGraphicUpdate, WorkingGraphicResponse, DayBase, DayCreate, DayUpdate, DayResponse
 from .attendance import AttendanceBase, AttendanceCreate, AttendanceUpdate, AttendanceResponse, AttendanceDataResponse
 from .user import UserBase, UserCreate, UserUpdate, UserResponse
