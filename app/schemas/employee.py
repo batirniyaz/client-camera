f804e6c522346b3099d6936440a246b7d21f6aec
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-08-20 16:53:26 +0500

    delete unnecessary fields and change position, filial and working_graphic fields to Response models

--- app/schemas/employee.py
+++ app/schemas/employee.py
@@ -1,13 +1,12 @@
 import datetime
 
 from pydantic import BaseModel, Field
-from typing import Optional, TYPE_CHECKING
+from typing import Optional
 
-# if TYPE_CHECKING:
-from app.schemas.employee_image import EmployeeImageResponse
-from app.schemas.position import PositionResponse
-from app.schemas.working_graphic import WorkingGraphicResponse
-from app.schemas.filial import FilialResponse
+from .employee_image import EmployeeImageResponse
+from .filial import FilialResponse
+from .position import PositionResponse
+from .working_graphic import WorkingGraphicResponse
 
 
 class EmployeeBase(BaseModel):
@@ -25,22 +24,22 @@ class EmployeeCreate(EmployeeBase):
 class EmployeeUpdate(EmployeeBase):
     name: Optional[str] = Field(None, description="The name of the employee")
     phone_number: Optional[str] = Field(None, description="The phone number of the employee")
-    position_id: Optional[int] = Field(None, description="The ID of the employee's position")
-    working_graphic_id: Optional[int] = Field(None, description="The ID of the employee's working graphic")
-    filial_id: Optional[int] = Field(None, description="The ID of the filial")
+    position_id: Optional[PositionResponse] = Field(None, description="The ID of the employee's position")
+    working_graphic_id: Optional[WorkingGraphicResponse] = Field(None, description="The ID of the employee's working graphic")
+    filial_id: Optional[FilialResponse] = Field(None, description="The ID of the filial")
 
     class Config:
         validate_assignment = True
 
 
 class EmployeeResponse(EmployeeBase):
+
     id: int = Field(..., description="The ID of the employee")
-    name: str = Field(..., description="The name of the employee")
-    position: 'PositionResponse' = Field(..., description="The position of the employee")
-    filial: 'FilialResponse' = Field(..., description="The filial of the employee")
-    phone_number: str = Field(..., description="The phone number of the employee")
-    working_graphic: 'WorkingGraphicResponse' = Field(None, description="The working graphic of the employee")
-    images: list['EmployeeImageResponse'] = Field([], description="A list of images associated with the employee")
+    position_id:  PositionResponse = Field(..., description="The position of the employee")
+    working_graphic_id: Optional[int] = Field(None, description="The ID of the employee's working graphic")
+    working_graphic: Optional[WorkingGraphicResponse] = Field(None, description="The working graphic of the employee")
+    filial_id: FilialResponse = Field(..., description="The filial of the employee")
+    images: list[EmployeeImageResponse] = Field([], description="A list of images associated with the employee")
 
     created_at: datetime.datetime = Field(..., description="The time the employee was created")
     updated_at: datetime.datetime = Field(..., description="The time the employee was updated")
@@ -51,43 +50,18 @@ class EmployeeResponse(EmployeeBase):
         json_schema_extra = {
             "example": {
                 "id": 1,
-                "name": "Employee 1",
-                "position": {
-                    "id": 1,
-                    "name": "Position 1",
-                    "employees": 10,
-                    "created_at": "2024-07-25 12:00:00"
-                },
-                "filial": {
-                    "id": 1,
-                    "name": "Filial 1",
-                    "created_at": "2024-07-25 12:00:00"
-                },
-                "phone_number": "1234567890",
-                "working_graphic": {
-                    "id": 1,
-                    "name": "Working Graphic 1",
-                    "days": [
-                        {
-                            "id": 1,
-                            "day": "Monday",
-                            "time_in": "08:00:00",
-                            "time_out": "17:00:00",
-                            "is_work_day": True,
-                            "created_at": "2024-07-25 12:00:00"
-                        }
-                    ],
-                    "created_at": "2024-07-25 12:00:00"
-                },
+                "name": "Batirniyaz",
+                "phone_number": "998905913873",
+                "position_id": 1,
+                "filial_id": 1,
                 "images": [
                     {
-                        "id": 1,
-                        "url": "https://example.com/image.jpg",
-                        "created_at": "2024-07-25 12:00:00"
+                        "employee_id": 1,
+                        "image_url": "http://example.com/image.jpg"
                     }
-                ],
-                "created_at": "2024-07-25 12:00:00"
+                ]
             }
         }
         arbitrary_types_allowed = True
         validate_assignment = True
+
