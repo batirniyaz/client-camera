e8f92223fbf097f9e38421a1499ea12cede4fe53
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-08-21 10:47:18 +0500

    change get methods of positions, will return employees list

--- app/crud/position.py
+++ app/crud/position.py
@@ -3,8 +3,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy.orm import joinedload
 
+from ..models import Employee
 from ..models.position import Position
-from ..schemas.position import PositionCreate, PositionUpdate
+from ..schemas.position import PositionCreate, PositionUpdate, PositionResponse
 
 
 async def create_position(db: AsyncSession, position: PositionCreate):
@@ -14,11 +15,6 @@ async def create_position(db: AsyncSession, position: PositionCreate):
         await db.commit()
         await db.refresh(db_position)
 
-        # result = await db.execute(
-        #     select(Position)
-        #     .options(joinedload(Position.employees))
-        #     .filter_by(id=db_position.id))
-
         return db_position
     except Exception as e:
         await db.rollback()
@@ -27,7 +23,34 @@ async def create_position(db: AsyncSession, position: PositionCreate):
 
 async def get_positions(db: AsyncSession, skip: int = 0, limit: int = 10):
     result = await db.execute(select(Position).offset(skip).limit(limit))
-    return result.scalars().all()
+    positions = result.scalars().all()
+
+    formatted_positions = []
+    for position in positions:
+        employees = await db.execute(select(Employee).filter_by(position_id=position.id))
+        employees = employees.scalars().all()
+
+        formatted_position = PositionResponse(
+            id=position.id,
+            name=position.name,
+            employees=[
+                {
+                    "id": employee.id,
+                    "name": employee.name,
+                    "phone_number": employee.phone_number,
+                    "position_id": employee.position_id,
+                    "working_graphic_id": employee.working_graphic_id,
+                    "filial_id": employee.filial_id,
+                    "created_at": employee.created_at,
+                    "updated_at": employee.updated_at,
+                } for employee in employees
+            ],
+            created_at=position.created_at,
+            updated_at=position.updated_at,
+        )
+        formatted_positions.append(formatted_position)
+
+    return formatted_positions
 
 
 async def get_position(db: AsyncSession, position_id: int):
@@ -35,7 +58,30 @@ async def get_position(db: AsyncSession, position_id: int):
     position = result.scalar_one_or_none()
     if not position:
         raise HTTPException(status_code=404, detail="Position not found")
-    return position
+
+    employees_result = await db.execute(select(Employee).filter_by(position_id=position.id))
+    employees = employees_result.scalars().all()
+
+    formatted_position = PositionResponse(
+        id=position.id,
+        name=position.name,
+        employees=[
+            {
+                "id": employee.id,
+                "name": employee.name,
+                "phone_number": employee.phone_number,
+                "position_id": employee.position_id,
+                "working_graphic_id": employee.working_graphic_id,
+                "filial_id": employee.filial_id,
+                "created_at": employee.created_at,
+                "updated_at": employee.updated_at,
+            } for employee in employees
+        ],
+        created_at=position.created_at,
+        updated_at=position.updated_at,
+    )
+
+    return formatted_position
 
 
 async def update_position(db: AsyncSession, position_id: int, position: PositionUpdate):
