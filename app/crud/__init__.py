d9f026554ffee3f017a55223a460d17808360f0a
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-08-22 15:08:56 +0500

    add delete_attendance to import file

--- app/crud/__init__.py
+++ app/crud/__init__.py
@@ -3,6 +3,6 @@ from .filial import create_filial, get_filials, get_filial, update_filial, delet
 from .position import create_position, get_positions, get_position, update_position, delete_position
 from .employee_image import create_employee_image, get_employees_images, get_employee_images, update_employee_image, delete_employee_image, get_employee_image
 from .working_graphic import create_working_graphic, create_day, get_working_graphics, get_working_graphic, update_working_graphic, delete_working_graphic, get_days
-from .attendance import create_attendance, get_attendances, get_commers
+from .attendance import create_attendance, get_attendances, get_commers, delete_attendance
 from .user import create_user, login_user, get_users, get_user, update_user, delete_user
 from .client import create_client, store_daily_report, get_daily_report
