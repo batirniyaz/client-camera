794a41c6f79a2e986d6e40ad87049e734f34844f
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-08-25 12:13:41 +0500

    change try except

--- app/crud/employee.py
+++ app/crud/employee.py
@@ -280,13 +280,8 @@ async def get_employee_deep(db: AsyncSession, employee_id: int, date: str):
         try:
             attendance_datetime = datetime.strptime(db_attendance.time, "%Y-%m-%d %H:%M:%S")
         except ValueError:
-            try:
-                attendance_datetime = datetime.strptime(db_attendance.time, "%Y-%m-%d")
-            except ValueError:
-                try:
-                    attendance_datetime = datetime.strptime(db_attendance.time, "%Y-%m-%d %H:%M:%S.%f")
-                except ValueError:
-                    raise HTTPException(status_code=400, detail=f"Time data {db_attendance.time} does not match format %Y-%m-%d %H:%M:%S")
+            raise HTTPException(status_code=400, detail=f"Time data {db_attendance.time} does not match format %Y-%m-%d %H:%M:%S")
+
         attendance_date = attendance_datetime.strftime("%Y-%m")
         if attendance_date == date:
             formatted_date_attendances.append(db_attendance)
