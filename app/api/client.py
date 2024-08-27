75d058ebdaa90402287983c1b8aa7a9d7a6b74f5
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-08-27 10:44:43 +0500

    from path parameter made three query parameters

--- app/api/client.py
+++ app/api/client.py
@@ -1,6 +1,7 @@
 from datetime import datetime
+from typing import Optional
 
-from fastapi import APIRouter, Depends, BackgroundTasks
+from fastapi import APIRouter, Depends, BackgroundTasks, Query, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from ..crud.client import create_client, get_daily_report, make_naive, store_daily_report
@@ -25,12 +26,33 @@ async def create_client_endpoint(background_tasks: BackgroundTasks, client: Clie
     return created_client
 
 
-@router.get("{date}")
-async def get_daily_report_endpoint(date: str, db: AsyncSession = Depends(get_db)):
+@router.get("/reports", response_model=dict)
+async def get_daily_report_endpoint(
+        date: Optional[str] = Query(None, alias="date", description="YYYY-MM-DD format"),
+        db: AsyncSession = Depends(get_db),
+        date1: Optional[str] = Query(None, alias="start_datetime", description="YYYY-MM-DD %H:%M format"),
+        date2: Optional[str] = Query(None, alias="end_datetime", description="YYYY-MM-DD %H:%M format")
+):
     """
     Get the daily report for the given date.
+    :param date2:
+    :param date1:
     :param date:
     :param db:
     :return:
     """
-    return await get_daily_report(db, date)
+    try:
+        date_obj = datetime.strptime(date, "%Y-%m-%d") if date else None
+
+        start_datetime = datetime.strptime(date1, "%Y-%m-%d %H:%M") if date1 else None
+        end_datetime = datetime.strptime(date2, "%Y-%m-%d %H:%M") if date2 else None
+
+        if not date and not (start_datetime and end_datetime):
+            raise HTTPException(status_code=400,
+                                detail="You must provide either a date or both start_datetime and end_datetime.")
+
+        report = await store_daily_report(db, date_obj, start_datetime, end_datetime)
+        return report
+
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=f"Invalid date format: {e}")
