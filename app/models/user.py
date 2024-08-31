cce1d465214ea5897c2ea13df953a219de8670cb
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-08-31 16:47:39 +0500

    fully change user model

--- app/models/user.py
+++ app/models/user.py
@@ -1,29 +1,16 @@
 import datetime
 
-# from fastapi_users import FastAPIUsers, models, schemas
-# from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
-# from fastapi_users.authentication import BearerTransport, JWTStrategy, AuthenticationBackend
-
 from sqlalchemy import text
 from sqlalchemy.orm import Mapped, mapped_column
 
-from ..database import Base
 
-
-class User(Base):
-    __tablename__ = 'users'
-
+class User:
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
-    name: Mapped[str] = mapped_column()
-    email: Mapped[str] = mapped_column(unique=True)
-    phone_number: Mapped[str] = mapped_column()
-    password: Mapped[str] = mapped_column()
+    email: Mapped[str] = mapped_column(index=True)
+    hashed_password: Mapped[str] = mapped_column()
+    is_active: Mapped[bool] = mapped_column()
+    is_superuser: Mapped[bool] = mapped_column()
+    is_verified: Mapped[bool] = mapped_column()
 
     created_at: Mapped[datetime.datetime] = mapped_column(server_default=text("TIMEZONE('utc', now())"))
     updated_at: Mapped[datetime.datetime] = mapped_column(server_default=text("TIMEZONE('utc', now())"), onupdate=text("TIMEZONE('utc', now())"))
-
-
-# class User(models.BaseUser, models.BaseOAuthAccountMixin):
-#     name: str
-#     email: str
-#     phone_number: str
