188fc077cf78ff7f81821a0336722b16d0127ad7
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-08-31 16:49:22 +0500

    partly change main.py file

--- app/main.py
+++ app/main.py
@@ -1,24 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.staticfiles import StaticFiles
 from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.database import engine, Base, SessionLocal
+from app.auth.auth import auth_backend, current_user, fastapi_users
+from app.auth.db import User
+from app.database import engine, Base, SessionLocal, create_session
 from app.api import router
+from app.auth.schema import UserRead, UserCreate
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
-    db = SessionLocal()
-    try:
-        print("Creating session")
+    async for db in create_session():
         yield db
-    finally:
-        await db.close()
-        print("Closing session")
 
 main_app = FastAPI(
     title="Employee Management API",
@@ -39,4 +34,21 @@ main_app.add_middleware(
 
 main_app.include_router(router)
 main_app.mount("/storage", StaticFiles(directory="app/storage"), name="storage")
+
+main_app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+main_app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+
+@main_app.get("/protected-route")
+def protected_route(user: User = Depends(current_user)):
+    return f"Hello, {user.email}"
 
