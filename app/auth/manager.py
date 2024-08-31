9e3707c53e5bbce49b9eb6412a74f24a129bb65b
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-08-31 16:48:29 +0500

    create and configure manager.py file to authorization

---
+++ app/auth/manager.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+from fastapi import Depends, Request
+from fastapi_users import BaseUserManager, IntegerIDMixin
+
+from app.auth.db import User, get_user_db
+from app.config import SECRET_AUTH
+
+SECRET = SECRET_AUTH
+
+
+class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
+    reset_password_token_secret = SECRET
+    verification_token_secret = SECRET
+
+    async def on_after_register(self, user: User, request: Optional[Request] = None):
+        print(f"User {user.id} has registered.")
+
+    async def on_after_forgot_password(
+        self, user: User, token: str, request: Optional[Request] = None
+    ):
+        print(f"User {user.id} has forgot their password. Reset token: {token}")
+
+    async def on_after_request_verify(
+        self, user: User, token: str, request: Optional[Request] = None
+    ):
+        print(f"Verification requested for user {user.id}. Verification token: {token}")
+
+
+async def get_user_manager(user_db=Depends(get_user_db)):
+    yield UserManager(user_db)
