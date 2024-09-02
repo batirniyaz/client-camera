45abf9158da0d11adb1deb662ec8a142eb7d6708
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-02 11:44:20 +0500

    configure some part of code

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -1,6 +1,9 @@
+from fastapi_users import FastAPIUsers
 from fastapi_users.authentication import BearerTransport, JWTStrategy, AuthenticationBackend
 
 from app.config import SECRET_AUTH
+from app.auth.db import User
+from app.auth.manager import get_user_manager
 
 bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
 
@@ -17,3 +20,11 @@ auth_backend = AuthenticationBackend(
     transport=bearer_transport,
     get_strategy=get_jwt_strategy,
 )
+
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+
+current_user = fastapi_users.current_user()
