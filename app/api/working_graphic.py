78087b59670caace6085a89147377b993144bfe8
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-02 16:34:02 +0500

    protect all working_graphic endpoint

--- app/api/working_graphic.py
+++ app/api/working_graphic.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.auth.auth import current_user
+from app.auth.db import User
 from ..crud.working_graphic import create_day, create_working_graphic, \
     get_working_graphics, get_working_graphic, update_working_graphic, delete_working_graphic, get_days, update_day, \
     delete_day
@@ -12,45 +14,107 @@ router = APIRouter()
 
 
 @router.post("/day", response_model=[])
-async def create_day_endpoint(day: DayCreate, working_graphic_id: int, db: AsyncSession = Depends(get_db)):
+async def create_day_endpoint(
+        day: DayCreate,
+        working_graphic_id: int,
+        db: AsyncSession = Depends(get_db),
+        user: User = Depends(current_user)
+):
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
+
     return await create_day(db, day, working_graphic_id)
 
 
 @router.post("/", response_model=[])
-async def create_working_graphic_endpoint(working_graphic: WorkingGraphicCreate, db: AsyncSession = Depends(get_db)):
+async def create_working_graphic_endpoint(
+        working_graphic: WorkingGraphicCreate,
+        db: AsyncSession = Depends(get_db),
+        user: User = Depends(current_user)
+):
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
+
     return await create_working_graphic(db, working_graphic)
 
 
 @router.get("/day/{working_graphic_id}", response_model=list[DayResponse])
-async def get_days_endpoint(working_graphic_id: int, db: AsyncSession = Depends(get_db)):
+async def get_days_endpoint(
+        working_graphic_id: int,
+        db: AsyncSession = Depends(get_db),
+        user: User = Depends(current_user)
+):
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
+
     return await get_days(db, working_graphic_id)
 
 
 @router.get("/", response_model=list[WorkingGraphicResponse])
-async def get_working_graphics_endpoint(skip: int = 0, limit: int = 10, db: AsyncSession = Depends(get_db)):
+async def get_working_graphics_endpoint(
+        skip: int = 0,
+        limit: int = 10,
+        db: AsyncSession = Depends(get_db),
+        user: User = Depends(current_user)
+):
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
     return await get_working_graphics(db, skip, limit)
 
 
 @router.get("/{working_graphic_id}", response_model=WorkingGraphicResponse)
-async def get_working_graphic_endpoint(working_graphic_id: int, db: AsyncSession = Depends(get_db)):
+async def get_working_graphic_endpoint(
+        working_graphic_id: int,
+        db: AsyncSession = Depends(get_db),
+        user: User = Depends(current_user)
+):
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
     return await get_working_graphic(db, working_graphic_id)
 
 
 @router.put("/{working_graphic_id}", response_model=WorkingGraphicResponse)
-async def update_working_graphic_endpoint(working_graphic_id: int, working_graphic: WorkingGraphicUpdate, db: AsyncSession = Depends(get_db)):
+async def update_working_graphic_endpoint(
+        working_graphic_id: int,
+        working_graphic: WorkingGraphicUpdate,
+        db: AsyncSession = Depends(get_db),
+        user: User = Depends(current_user)
+):
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
     return await update_working_graphic(db, working_graphic_id, working_graphic)
 
 
 @router.put("/day/{day_id}", response_model=[])
-async def update_day_endpoint(day_id: int, day: DayUpdate, db: AsyncSession = Depends(get_db)):
+async def update_day_endpoint(
+        day_id: int,
+        day: DayUpdate,
+        db: AsyncSession = Depends(get_db),
+        user: User = Depends(current_user)
+):
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
+
     return await update_day(db, day_id, day)
 
 
 @router.delete("/{working_graphic_id}")
-async def delete_working_graphic_endpoint(working_graphic_id: int, db: AsyncSession = Depends(get_db)):
+async def delete_working_graphic_endpoint(
+        working_graphic_id: int,
+        db: AsyncSession = Depends(get_db),
+        user: User = Depends(current_user)
+):
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
     return await delete_working_graphic(db, working_graphic_id)
 
 
 @router.delete("/day/{day_id}")
-async def delete_day_endpoint(day_id: int, db: AsyncSession = Depends(get_db)):
+async def delete_day_endpoint(
+        day_id: int,
+        db: AsyncSession = Depends(get_db),
+        user: User = Depends(current_user)
+):
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
     return await delete_day(db, day_id)
