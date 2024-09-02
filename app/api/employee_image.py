892d0a8fbe2da9fdd83da8e0eb421600d522aa0b
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-02 16:33:23 +0500

    protect all employee_image endpoint

--- app/api/employee_image.py
+++ app/api/employee_image.py
@@ -1,8 +1,10 @@
-import os
 from typing import List
 
-from fastapi import APIRouter, Depends, File, UploadFile
+from fastapi import APIRouter, Depends, File, UploadFile, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.auth.auth import current_user
+from app.auth.db import User
 from ..crud.employee_image import (
     create_employee_image,
     get_employee_images,
@@ -21,54 +23,81 @@ router = APIRouter()
 async def create_employee_image_endpoint(
         employee_id: int,
         file: UploadFile = File(...),
-        db: AsyncSession = Depends(get_db)
+        db: AsyncSession = Depends(get_db),
+        user: User = Depends(current_user),
 ):
 
     """
     Store an image for the employee with the given ID.
+    :param user:
     :param employee_id:
     :param file:
     :param db:
     :return:
     """
 
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
+
     return await create_employee_image(db, employee_id, file)
 
 
 @router.get("/images", response_model=List[EmployeeImageResponse])
-async def get_employees_images_endpoint(db: AsyncSession = Depends(get_db)):
+async def get_employees_images_endpoint(
+        db: AsyncSession = Depends(get_db),
+        user: User = Depends(current_user)):
 
     """
     Get an image for the employee with the given ID.
+    :param user:
     :param employee_id:
     :param db:
     :return:
     """
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
     return await get_employees_images(db)
 
 
 @router.get("/{employee_id}/images/", response_model=List[EmployeeImageResponse])
-async def get_employee_images_endpoint(employee_id: int, db: AsyncSession = Depends(get_db)):
+async def get_employee_images_endpoint(
+        employee_id: int,
+        db: AsyncSession = Depends(get_db),
+        user: User = Depends(current_user)):
 
     """
     Get an image for the employee with the given ID.
+    :param user:
     :param employee_id:
     :param employee_image_id:
     :param db:
     :return:
     """
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
     return await get_employee_images(db, employee_id)
 
 
 @router.get("/{employee_id}/images/{employee_image_id}", response_model=EmployeeImageResponse)
-async def get_employee_image_endpoint(employee_id: int, employee_image_id: int, db: AsyncSession = Depends(get_db)):
+async def get_employee_image_endpoint(
+        employee_id: int,
+        employee_image_id: int,
+        db: AsyncSession = Depends(get_db),
+        user: User = Depends(current_user),
+):
     """
     Get an image for the employee with the given ID.
+    :param user:
     :param employee_id:
     :param employee_image_id:
     :param db:
     :return:
     """
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
     return await get_employee_image(db, employee_id, employee_image_id)
 
 
@@ -77,28 +106,41 @@ async def update_employee_image_endpoint(
         employee_id: int,
         employee_image_id: int,
         employee_image: EmployeeImageUpdate,
-        db: AsyncSession = Depends(get_db)
+        db: AsyncSession = Depends(get_db),
+        user: User = Depends(current_user),
 ):
 
     """
     Update an image for the employee with the given ID.
+    :param user:
     :param employee_id:
     :param employee_image_id:
     :param employee_image:
     :param db:
     :return:
     """
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
     return await update_employee_image(db, employee_id, employee_image_id, employee_image)
 
 
 @router.delete("/{employee_id}/images/{employee_image_id}")
-async def delete_employee_image_endpoint(employee_id: int, employee_image_id: int, db: AsyncSession = Depends(get_db)):
+async def delete_employee_image_endpoint(
+        employee_id: int,
+        employee_image_id: int,
+        db: AsyncSession = Depends(get_db),
+        user: User = Depends(current_user),
+):
 
     """
     Delete an image for the employee with the given ID.
+    :param user:
     :param employee_id:
     :param employee_image_id:
     :param db:
     :return:
     """
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
     return await delete_employee_image(db, employee_id, employee_image_id)
