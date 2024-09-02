9a4da0fd15583e92c34ae14e141107031babe170
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-02 16:32:49 +0500

    delete user crud import

--- app/crud/__init__.py
+++ app/crud/__init__.py
@@ -4,5 +4,4 @@ from .position import create_position, get_positions, get_position, update_posit
 from .employee_image import create_employee_image, get_employees_images, get_employee_images, update_employee_image, delete_employee_image, get_employee_image
 from .working_graphic import create_working_graphic, create_day, get_working_graphics, get_working_graphic, update_working_graphic, delete_working_graphic, get_days, update_day, delete_day
 from .attendance import create_attendance, get_attendances, get_commers_by_filial, get_commers_filials, get_commers_percentage, delete_attendance, get_daily_attendance, get_attend_day
-from .user import create_user, login_user, get_users, get_user, update_user, delete_user
 from .client import create_client, store_daily_report, get_daily_report
