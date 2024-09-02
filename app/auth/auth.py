ae2b5cf6a15e497dfb7621ea5499c1521ec08e30
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-02 11:12:17 +0500

    create and configure base configs of auth

---
+++ app/auth/auth.py
@@ -0,0 +1,19 @@
+from fastapi_users.authentication import BearerTransport, JWTStrategy, AuthenticationBackend
+
+from app.config import SECRET_AUTH
+
+bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
+
+
+SECRET = SECRET_AUTH
+
+
+def get_jwt_strategy() -> JWTStrategy:
+    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+
+
+auth_backend = AuthenticationBackend(
+    name="jwt",
+    transport=bearer_transport,
+    get_strategy=get_jwt_strategy,
+)
