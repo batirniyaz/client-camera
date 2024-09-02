a6a89a770f52d417c2327e2b790c018efdcb884e
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-02 16:33:15 +0500

    protect all client endpoint

--- app/api/client.py
+++ app/api/client.py
@@ -1,10 +1,12 @@
 from datetime import datetime
 from typing import Optional
 
-from fastapi import APIRouter, Depends, BackgroundTasks, Query, HTTPException
+from fastapi import APIRouter, Depends, BackgroundTasks, Query, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from ..crud.client import create_client, get_daily_report, make_naive, store_daily_report
+from app.auth.auth import current_user
+from app.auth.db import User
+from ..crud.client import create_client, store_daily_report
 from ..schemas import ClientCreate, ClientResponse
 from ..database import get_db
 
@@ -12,7 +14,12 @@ router = APIRouter()
 
 
 @router.post("/", response_model=ClientResponse)
-async def create_client_endpoint(background_tasks: BackgroundTasks, client: ClientCreate, db: AsyncSession = Depends(get_db)):
+async def create_client_endpoint(
+        background_tasks: BackgroundTasks,
+        client: ClientCreate,
+        db: AsyncSession = Depends(get_db),
+        user: User = Depends(current_user)
+):
     """
         Create a new client with the given details.
 
@@ -21,6 +28,8 @@ async def create_client_endpoint(background_tasks: BackgroundTasks, client: Clie
         - **age**: The age of the client
         - **score**: The score of the client
         """
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
 
     created_client = await create_client(db, client, background_tasks)
     return created_client
@@ -31,16 +40,22 @@ async def get_daily_report_endpoint(
         date: Optional[str] = Query(None, alias="date", description="YYYY-MM-DD format"),
         db: AsyncSession = Depends(get_db),
         date1: Optional[str] = Query(None, alias="start_datetime", description="YYYY-MM-DD %H:%M format"),
-        date2: Optional[str] = Query(None, alias="end_datetime", description="YYYY-MM-DD %H:%M format")
+        date2: Optional[str] = Query(None, alias="end_datetime", description="YYYY-MM-DD %H:%M format"),
+        user: User = Depends(current_user)
 ):
     """
     Get the daily report for the given date.
+    :param user:
     :param date2:
     :param date1:
     :param date:
     :param db:
     :return:
     """
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
+
     try:
         date_obj = datetime.strptime(date, "%Y-%m-%d") if date else None
 
