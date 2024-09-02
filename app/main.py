f2fb423e13c11d9f09ba25e70b4cb624a4e2121b
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-02 11:13:10 +0500

    add endpoint os login logout and register

--- app/main.py
+++ app/main.py
@@ -2,9 +2,20 @@ from fastapi import FastAPI, Depends
 from fastapi.staticfiles import StaticFiles
 from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi_users import FastAPIUsers
 
 from app.api import router
 from app.database import create_session
+
+from app.auth.db import User
+from app.auth.auth import auth_backend
+from app.auth.manager import get_user_manager
+from app.auth.schemas import UserRead, UserCreate
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
 
 
 @asynccontextmanager
@@ -31,5 +42,24 @@ main_app.add_middleware(
 
 main_app.include_router(router)
 main_app.mount("/storage", StaticFiles(directory="app/storage"), name="storage")
+
+main_app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+
+main_app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+current_user = fastapi_users.current_user()
+
+@main_app.get("/protected-route")
+def protected_route(user: User = Depends(current_user)):
+    return f"Hello, {user.email}"
 
 
