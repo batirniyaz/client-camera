2518a443bf8ae1bc8ba76c4afaffac08f42764b4
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-02 11:12:53 +0500

    change partly the model

--- app/auth/db.py
+++ app/auth/db.py
@@ -2,14 +2,22 @@ from typing import AsyncGenerator
 
 from fastapi import Depends
 from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from sqlalchemy import String, Boolean
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Mapped, mapped_column
+
 from app.database import DATABASE_URL, Base, SessionLocal
 
 DATABASE_URL = DATABASE_URL
 
 
 class User(SQLAlchemyBaseUserTable[int], Base):
-    pass
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    email: Mapped[str] = mapped_column(String(length=320), unique=True, index=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String(length=1024), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+    is_superuser: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    is_verified: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
